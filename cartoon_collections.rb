46a223aabdf7502dc5cc5ae75952b66da19a2b3f
Brian Donnoe <dunow33@hotmail.com>
2020-08-04 07:51:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,10 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map do |
+  words = []
+  
+  array.map do |word|
+    words << "word.upcase"
 end
 
 def long_planeteer_calls# code an argument here
